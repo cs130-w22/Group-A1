c79d5946202b37f2e65904ae5e5f8cb5caf68455
Alvin Nguyen <alvinonguyen00@gmail.com>
2022-03-04 20:56:22 -0800

    added polls unit test template file

---
+++ server/tests/polls.unit.test.js
@@ -0,0 +1,23 @@
+describe('Adding poll', () => {
+  it.todo('');
+});
+
+describe('Edit poll', () => {
+  it.todo('');
+});
+
+describe('Delete poll', () => {
+  it.todo('');
+});
+
+describe('Add poll option', () => {
+  it.todo('');
+});
+
+describe('Vote for poll option', () => {
+  it.todo('');
+});
+
+describe('Delete poll option', () => {
+  it.todo('');
+});
