d4d42270385382544be54fc80d7b9325732d068e
Alvin Nguyen <50280292+ultramarathon@users.noreply.github.com>
2022-03-07 07:19:14 -0800

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 // routers
 const loginRouter = require('./routes/login');
@@ -18,6 +19,7 @@ const createServer = (middleware) => {
   app.use(cors());
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
+  app.use(express.static(path.join('..', 'client', 'build')));
 
   // other middleware
   if (middleware != null && Array.isArray(middleware)) {
