247ebb6f6db96ada048682552beca75e45f08aec
Elyse Yao <elyseyao@gmail.com>
2022-03-07 22:13:34 -0800

    actually fix deployment issue lol

--- server/app.js
+++ server/app.js
@@ -17,7 +17,7 @@ const createServer = (middleware) => {
   const app = express();
   app.use(cors({
     credentials: true,
-    origin: 'https://cya-client-cs130.herokuapp.com/',
+    origin: 'https://cya-client-cs130.herokuapp.com',
   }));
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
